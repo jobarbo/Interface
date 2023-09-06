ff15e003502d43a74c8e8f6a37dafba49721fdeb
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-09-06 15:18:47 -0400

    new main

--- debug.js
+++ debug.js
@@ -9,46 +9,32 @@ function defineBones() {
 	bones.push(['RIGHT_EYE', 'RIGHT_EYE_INNER']);
 
 	// arms
-	/* 	bones.push(['LEFT_SHOULDER', 'RIGHT_SHOULDER']);
+	bones.push(['LEFT_SHOULDER', 'RIGHT_SHOULDER']);
 
 	bones.push(['LEFT_SHOULDER', 'LEFT_ELBOW']);
-	bones.push(['LEFT_ELBOW', 'LEFT_WRIST']); */
-
+	bones.push(['LEFT_ELBOW', 'LEFT_WRIST']);
 	bones.push(['LEFT_WRIST', 'LEFT_THUMB']);
-	/* 	bones.push(['LEFT_WRIST', 'LEFT_INDEX']);
-	bones.push(['LEFT_WRIST', 'LEFT_PINKY']); */
-	/*
+	bones.push(['LEFT_WRIST', 'LEFT_INDEX']);
+	bones.push(['LEFT_WRIST', 'LEFT_PINKY']);
+
 	bones.push(['RIGHT_SHOULDER', 'RIGHT_ELBOW']);
-	bones.push(['RIGHT_ELBOW', 'RIGHT_WRIST']); */
+	bones.push(['RIGHT_ELBOW', 'RIGHT_WRIST']);
 	bones.push(['RIGHT_WRIST', 'RIGHT_THUMB']);
-	/* 	bones.push(['RIGHT_WRIST', 'RIGHT_INDEX']);
-	bones.push(['RIGHT_WRIST', 'RIGHT_PINKY']); */
+	bones.push(['RIGHT_WRIST', 'RIGHT_INDEX']);
+	bones.push(['RIGHT_WRIST', 'RIGHT_PINKY']);
 
 	// legs
-	//bones.push(['LEFT_HIP', 'RIGHT_HIP']);
-	bones.push(['LEFT_HIP', 'LEFT_ELBOW']);
-	bones.push(['RIGHT_HIP', 'RIGHT_ELBOW']);
-	bones.push(['LEFT_HIP', 'LEFT_WRIST']);
-	bones.push(['RIGHT_HIP', 'RIGHT_WRIST']);
+	bones.push(['LEFT_HIP', 'RIGHT_HIP']);
 
 	bones.push(['LEFT_HIP', 'LEFT_KNEE']);
-	bones.push(['LEFT_HIP', 'LEFT_SHOULDER']);
 	bones.push(['LEFT_KNEE', 'LEFT_ANKLE']);
 	bones.push(['LEFT_ANKLE', 'LEFT_HEEL']);
 	bones.push(['LEFT_HEEL', 'LEFT_FOOT_INDEX']);
 
 	bones.push(['RIGHT_HIP', 'RIGHT_KNEE']);
-	bones.push(['RIGHT_HIP', 'RIGHT_SHOULDER']);
 	bones.push(['RIGHT_KNEE', 'RIGHT_ANKLE']);
 	bones.push(['RIGHT_ANKLE', 'RIGHT_HEEL']);
 	bones.push(['RIGHT_HEEL', 'RIGHT_FOOT_INDEX']);
-
-	/* 	bones.push(['LEFT_ANKLE', 'LEFT_WRIST']);
-	bones.push(['RIGHT_ANKLE', 'RIGHT_WRIST']);
-	bones.push(['LEFT_ANKLE', 'LEFT_WRIST']);
-	bones.push(['RIGHT_ANKLE', 'RIGHT_WRIST']);
-	bones.push(['RIGHT_WRIST', 'RIGHT_EYE']);
-	bones.push(['LEFT_WRIST', 'LEFT_EYE']); */
 }
 
 function drawBones(frame) {
@@ -65,37 +51,81 @@ function drawBones(frame) {
 			j2 = remapPos(joint2.x, joint2.y);
 		}
 		if (j1 && j2) {
-			// draw a custom shape that follows the line and joints of the body
-			strokeWeight(0.5);
-			stroke(random(40), random(40), random([10, 30, 40, 40, 50, 60]), a);
-			//divide the line in 10 parts
-			let x1 = j1.x;
-			let y1 = j1.y;
-			let x2 = j2.x;
-			let y2 = j2.y;
-			let dx = x2 - x1;
-			let dy = y2 - y1;
-			let steps = 10;
-			let stepX = dx / steps;
-			let stepY = dy / steps;
-			let x = x1;
-			let y = y1;
-			for (let i = 0; i < steps; i++) {
-				let x1 = x + random(-10, 10);
-				let y1 = y + random(-10, 10);
-				let x2 = x + stepX + random(-10, 10);
-				let y2 = y + stepY + random(-10, 10);
-				line(x1, y1, x2, y2);
-				x += stepX;
-				y += stepY;
+			stroke(0, 0, 0, 100);
+			line(j1.x + random(-1, 1), j1.y + random(-1, 1), j2.x + random(-1, 1), j2.y + random(-1, 1));
+		}
+	}
+}
+
+function drawJoints(frame) {
+	let joints = MCdata[frame];
+	let j;
+	let previousJoints = MCdata[frame];
+	let pj;
+
+	if (frame > 0) {
+		previousJoints = MCdata[frame - 1];
+	}
+	for (let i = 0; i < joints.length; i++) {
+		let {x, y, type, name} = joints[i];
+
+		// In this example we use only body joints, not detailed hands joints
+		if (type === 'body') {
+			let {x: px, y: py} = previousJoints[i];
+			j = remapPos(x, y);
+			pj = remapPos(px, py);
+
+			// convert j and pj to vectors
+			j = createVector(j.x, j.y);
+			pj = createVector(pj.x, pj.y);
+
+			// calculate the distance between the two vectors
+
+			let sw = 1;
+
+			if (j) {
+				//circle(j.x, j.y, 50);
+
+				strokeWeight(sw);
+				stroke(0, 50, 100);
+				noFill();
+				ellipse(j.x, j.y, 50, 50);
 			}
 		}
 	}
 }
 
+// make a function to get the position of all the joints in an array
+function get_all_joint_pos(frame) {
+	let joints = MCdata[frame];
+	let j = [];
+	for (let i = 0; i < joints.length; i++) {
+		let {x, y, type, name} = joints[i];
+		if (type === 'body') {
+			j.push(remapPos(x, y));
+		}
+	}
+	return j;
+}
+
+// make a function to get the position of the joints
+function get_joint_pos(frame, joint_name) {
+	let joints = MCdata[frame];
+	let j;
+	for (let i = 0; i < joints.length; i++) {
+		let {x, y, type, name} = joints[i];
+		if (type === 'body') {
+			if (name === joint_name) {
+				j = remapPos(x, y);
+			}
+		}
+	}
+	return j;
+}
+
 function remapPos(x, y) {
 	let x1 = map(x, 0, 1, -width / 2, width / 2) * scaleMotionData + width / 1.5;
-	let y1 = map(y, 0, 1, -height / 2, height / 2) * scaleMotionData + height / 2;
+	let y1 = map(y, 0, 1, -height / 2, height / 2) * scaleMotionData + height / 1.5;
 	return {x: x1, y: y1};
 }
 
@@ -111,7 +141,7 @@ function checkMIDI() {
 	}
 	if (kname == '35') {
 		b = int(map(int(kval), 0, 100, 0, 100, true));
-		b2 = int(map(int(kval), 0, 100, 100, 0, true));
+		b2 = int(map(int(kval), 0, 100, 100, 100, true));
 	}
 	if (kname == '36') {
 		frame = int(map(int(kval), 0, 100, 0, framesMax / 4, true));
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let MCdata; // Object to store motion capture data
 let bones = []; // Bones are connections between joints
 let frame = 0; // Keeps the currently displayed frame
 let framesMax; // Maximum number of frames, to loop the animation
-let scaleMotionData = 2.25; // Scale the figure on screen by a factor, "zoom"
+let scaleMotionData = 1.25; // Scale the figure on screen by a factor, "zoom"
 
 let a = 100;
 let h = 0;
@@ -15,8 +15,8 @@ let b2 = 100;
 
 function preload() {
 	// Import motion capture data
-	MCdata = loadJSON('Moonlight_frontal_by_Juliano_Nunes.json');
-	//MCdata = loadJSON('Edge_Me_Away_by_Emrecan_Tanis.json');
+	//MCdata = loadJSON('Moonlight_frontal_by_Juliano_Nunes.json');
+	MCdata = loadJSON('Edge_Me_Away_by_Emrecan_Tanis.json');
 	console.log(MCdata);
 }
 
@@ -29,73 +29,25 @@ function setup() {
 	defineBones();
 	strokeWeight(1);
 	background(50, 5, 100);
+
+	let joints_pos = get_all_joint_pos(frame);
 }
 
 function draw() {
+	background(50, 5, 100);
 	// Draw joints
 	noStroke();
-	fill(200, 0, 100, 10);
 	drawJoints(frame);
 
 	// Draw bones
 	noFill();
 	strokeWeight(0.5);
-	stroke(0, 0, 0, a);
+	stroke(0, 0, 100, 100);
 	drawBones(frame);
 
 	// Loop the animation
 	checkMIDI();
 	if (frame >= framesMax) {
 		frame = 0;
-	}
-}
-
-// Reposition in the middle of the screen and scale
-
-function drawJoints(frame) {
-	let joints = MCdata[frame];
-	let j;
-	let previousJoints = MCdata[frame];
-	let pj;
-
-	if (frame > 0) {
-		previousJoints = MCdata[frame - 1];
-	}
-	for (let i = 0; i < joints.length; i++) {
-		let {x, y, type, name} = joints[i];
-
-		// In this example we use only body joints, not detailed hands joints
-		if (type === 'body') {
-			let {x: px, y: py} = previousJoints[i];
-			j = remapPos(x, y);
-			pj = remapPos(px, py);
-
-			// convert j and pj to vectors
-			j = createVector(j.x, j.y);
-			pj = createVector(pj.x, pj.y);
-
-			// calculate the distance between the two vectors
-			let d = p5.Vector.dist(j, pj);
-			let sw = 0.1;
-
-			if (j) {
-				sw = map(d, 0, 100, 0, 3);
-				//circle(j.x, j.y, 50);
-
-				strokeWeight(sw);
-
-				for (let i = 0; i < 100; i++) {
-					let v = p5.Vector.random2D();
-					v.mult(random(2, random(10, d)));
-
-					fill(h, s, b, a);
-					// create a curved line between the two vectors
-					stroke(h, s, b, a);
-					line(j.x, j.y, j.x + v.x, j.y + v.y);
-					stroke(h, s, b2, a);
-					line(j.x + 2, j.y + 2, j.x + v.x + 2, j.y + v.y + 2);
-				}
-			}
-		}
 	}
 }
