d76ae748fc1aabcc1a61bd0a2505f6f5d4d63416
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-09-06 18:28:33 -0400

    jdl alike

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 // Example to import motion capture data for the Interfaces project
 // Motion Capture data © http://interfaces.7pc.de/
 
+let seed;
 let MCdata; // Object to store motion capture data
 let bones = []; // Bones are connections between joints
 let frame = 0; // Keeps the currently displayed frame
+let lastFrame = frame;
 let framesMax; // Maximum number of frames, to loop the animation
-let scaleMotionData = 1.25; // Scale the figure on screen by a factor, "zoom"
+let scaleMotionData = 1; // Scale the figure on screen by a factor, "zoom"
 
+let num = 100;
+let w = 1;
 let a = 100;
 let h = 0;
 let s = 0;
 let b = 0;
 let b2 = 100;
+
+let particles = [];
+let scl1;
+let scl2;
+let ang1;
+let ang2;
+let rseed;
+let nseed;
+let xMin;
+let xMax;
+let yMin;
+let yMax;
+let startTime;
+let maxFrames = 60;
+let C_WIDTH;
+let MULTIPLIER;
+
+let allParticlesDrawn = false;
 
 function preload() {
 	// Import motion capture data
@@ -21,32 +43,99 @@ function preload() {
 }
 
 function setup() {
-	createCanvas(16 * 100, 16 * 100);
+	C_WIDTH = min(windowWidth, windowHeight);
+	MULTIPLIER = C_WIDTH / 1600;
+	c = createCanvas(C_WIDTH, C_WIDTH * 1.375);
 	colorMode(HSB, 360, 100, 100, 100);
+
 	frameRate(30);
 	framesMax = Object.keys(MCdata).length;
-	console.log(framesMax);
-	defineBones();
-	strokeWeight(1);
-	background(50, 5, 100);
+	seed = random(100000);
 
-	let joints_pos = get_all_joint_pos(frame);
+	h = random(360);
+
+	background(50, 5, 100);
+	defineBones();
 }
 
 function draw() {
-	background(50, 5, 100);
-	// Draw joints
-	noStroke();
-	drawJoints(frame);
+	console.log(frame);
+	//background(50, 5, 100);
 
-	// Draw bones
+	// Draw joints
+	/* 	noStroke();
+	drawJoints(frame); */
+
+	/* 	// Draw bones
 	noFill();
 	strokeWeight(0.5);
 	stroke(0, 0, 100, 100);
-	drawBones(frame);
+	drawBones(frame); */
 
 	// Loop the animation
 	checkMIDI();
+
+	// if frame value changes, destroy all particles and create new ones
+	if (frame != lastFrame) {
+		let joints_pos = get_all_joint_pos(frame);
+
+		for (let i = 0; i < joints_pos.length; i++) {
+			let {x, y} = joints_pos[i];
+			x = x + random(-1, 1);
+			y = y + random(-1, 1);
+
+			let hue = h;
+			for (let i = 0; i < num; i++) {
+				scl1 = random([0.00095, 0.001, 0.0011, 0.0012]);
+				scl2 = scl1;
+
+				ang1 = int(random([1, 5, 10, 20, 40, 80, 160, 320, 640, 1280]));
+				ang2 = int(random([1, 5, 10, 20, 40, 80, 160, 320, 640, 1280]));
+
+				xRandDivider = random([0.08, 0.09, 0.1, 0.11, 0.12]);
+				yRandDivider = xRandDivider;
+				xMin = -0.01;
+				xMax = 1.01;
+				yMin = -0.01;
+				yMax = 1.01;
+				let initHue = hue + random(-1, 1);
+				initHue = initHue > 360 ? initHue - 360 : initHue < 0 ? initHue + 360 : initHue;
+				let p = new Particle(
+					x,
+					y,
+					initHue,
+					scl1 / MULTIPLIER,
+					scl2 / MULTIPLIER,
+					ang1 * MULTIPLIER,
+					ang2 * MULTIPLIER,
+					xMin,
+					xMax,
+					yMin,
+					yMax,
+					xRandDivider,
+					yRandDivider,
+					seed
+				);
+				particles.push(p);
+			}
+		}
+		lastFrame = frame;
+	}
+
+	// delete particles if they are too old or alpha is at 0
+	for (let i = particles.length - 1; i >= 0; i--) {
+		let p = particles[i];
+		if (p.a < 0) {
+			particles.splice(i, 1);
+		}
+	}
+
+	for (let i = 0; i < particles.length; i++) {
+		let p = particles[i];
+		p.update();
+		p.show();
+	}
+
 	if (frame >= framesMax) {
 		frame = 0;
 	}
---
+++ particles.js
@@ -0,0 +1,120 @@
+class Particle {
+	constructor(x, y, hue, scl1, scl2, ang1, ang2, xMin, xMax, yMin, yMax, xRandDivider, yRandDivider, seed) {
+		this.x = x;
+		this.y = y;
+		this.initHue = hue;
+		this.initSat = random([0, 10, 20, 20, 20, 30, 40, 40, 60, 80, 80, 90]);
+		this.initBri = random([0, 10, 20, 20, 40, 40, 60, 70, 80, 90, 100]);
+		this.initAlpha = 100;
+		this.initS = 0.65 * MULTIPLIER;
+		this.hue = this.initHue;
+		this.sat = this.initSat;
+		this.bri = this.initBri;
+		this.a = this.initAlpha;
+		this.hueStep = 1;
+		this.s = this.initS;
+		this.scl1 = scl1;
+		this.scl2 = scl2;
+		this.ang1 = ang1;
+		this.ang2 = ang2;
+		this.seed = seed;
+		this.xRandDivider = xRandDivider;
+		this.yRandDivider = yRandDivider;
+		this.xRandSkipper = 0;
+		this.yRandSkipper = 0;
+		this.xRandSkipperVal = 0;
+		this.yRandSkipperVal = 0;
+		this.xMin = xMin;
+		this.xMax = xMax;
+		this.yMin = yMin;
+		this.yMax = yMax;
+		this.oct = 4;
+		this.centerX = width / 2;
+		this.centerY = height / 2;
+		this.borderX = width / 3;
+		this.borderY = height / 3;
+		this.clampvaluearray = [0.15, 0.25, 0.25, 0.015];
+		this.uvalue = random([5, 7, 10, 12, 15, 20, 40]);
+	}
+
+	update() {
+		// make the particles move according to noise;
+		let p = superCurve(
+			this.x,
+			this.y,
+			this.scl1,
+			this.scl2,
+			this.ang1,
+			this.ang2,
+			this.seed,
+			this.oct,
+			this.clampvaluearray,
+			this.uvalue
+		);
+
+		this.xRandSkipper = random(-this.xRandSkipperVal * MULTIPLIER, this.xRandSkipperVal * MULTIPLIER);
+		this.yRandSkipper = random(-this.xRandSkipperVal * MULTIPLIER, this.xRandSkipperVal * MULTIPLIER);
+		this.x += (p.x * MULTIPLIER) / this.xRandDivider + this.xRandSkipper;
+		this.y += (p.y * MULTIPLIER) / this.yRandDivider + this.yRandSkipper;
+
+		this.x =
+			this.x <= this.centerX - this.borderX
+				? this.centerX + this.borderX + random(-4 * MULTIPLIER, 0)
+				: this.x >= this.centerX + this.borderX
+				? this.centerX - this.borderX + random(0, 4 * MULTIPLIER)
+				: this.x;
+		this.y =
+			this.y <= this.centerY - this.borderY
+				? this.centerY + this.borderY + random(-4 * MULTIPLIER, 0)
+				: this.y >= this.centerY + this.borderY
+				? this.centerY - this.borderY + random(0, 4 * MULTIPLIER)
+				: this.y;
+
+		let pxy = p.x - p.y;
+		this.hue += mapValue(pxy, -this.uvalue * 2, this.uvalue * 2, -this.hueStep, this.hueStep, true);
+		this.hue = this.hue > 360 ? this.hue - 360 : this.hue < 0 ? this.hue + 360 : this.hue;
+
+		this.a -= 0.5;
+	}
+
+	show() {
+		fill(this.hue, this.sat, this.bri, this.a);
+		noStroke();
+		rect(this.x, this.y, this.s);
+	}
+}
+
+function superCurve(x, y, scl1, scl2, ang1, ang2, seed, octave, clampvalueArr, uvalue) {
+	let nx = x,
+		ny = y,
+		a1 = ang1,
+		a2 = ang2,
+		scale1 = scl1,
+		scale2 = scl2,
+		dx,
+		dy;
+
+	dx = oct(nx, ny, scale1, 0, octave);
+	dy = oct(nx, ny, scale2, 2, octave);
+	nx += dx * a1;
+	ny += dy * a2;
+
+	dx = oct(nx, ny, scale1, 1, octave);
+	dy = oct(nx, ny, scale2, 3, octave);
+	nx += dx * a1;
+	ny += dy * a2;
+
+	dx = oct(nx, ny, scale1, 1, octave);
+	dy = oct(nx, ny, scale2, 2, octave);
+	nx += dx * a1;
+	ny += dy * a2;
+
+	let un = oct(nx, ny, scale1, 0, octave);
+	let vn = oct(nx, ny, scale2, 1, octave);
+
+	let u = mapValue(un, -clampvalueArr[0], clampvalueArr[1], -uvalue, uvalue, true);
+	let v = mapValue(vn, -clampvalueArr[2], clampvalueArr[3], -uvalue, uvalue, true);
+
+	let p = createVector(u, v);
+	return p;
+}
